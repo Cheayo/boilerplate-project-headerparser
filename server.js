ff027383f71ce89afd503a64596a165ea8729b39
Lessa Winston <lessa.winston@protonmail.com>
2021-06-30 21:19:33 +0000

    added language to response

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 
 
 app.get("/api/whoami", function (req, res) {
-  res.json({ipaddress: req.ip});
+  res.json({ipaddress: req.ip, language: req.header("Accept-Language")});
 });
 
 
